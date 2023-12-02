eadc982f183ee983555476d1c7bb23d47d1a0e7b
Dishan <dishanddd@gmail.com>
2023-12-02 21:12:28 +0530

    2023 Day 1 - Part 2 - Solution

--- tests/aockt/y2023/Y2023D01Test.kt
+++ tests/aockt/y2023/Y2023D01Test.kt
@@ -5,12 +5,24 @@ import io.github.jadarma.aockt.test.AdventSpec
 
 @AdventDay(2023, 1, "Question 1")
 class Y2023D01Test : AdventSpec<Y2023D01>({
-    partOne {
+    partOne(enabled = false) {
         """
             1abc2
             pqr3stu8vwx
             a1b2c3d4e5f
             treb7uchet
         """.trimIndent() shouldOutput 142
+    }
+
+    partTwo {
+        """
+            two1nine
+            eightwothree
+            abcone2threexyz
+            xtwone3four
+            4nineeightseven2
+            zoneight234
+            7pqrstsixteen
+        """.trimIndent() shouldOutput 281
     }
 })
