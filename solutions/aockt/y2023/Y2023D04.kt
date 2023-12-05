d988f8c0de3b4d38e90af34c4518b3133dcc61de
Dishan <dishanddd@gmail.com>
2023-12-05 17:53:32 +0530

    2023 Day 4 - Part 2 - Solution

--- solutions/aockt/y2023/Y2023D04.kt
+++ solutions/aockt/y2023/Y2023D04.kt
@@ -2,12 +2,39 @@ package aockt.y2023
 
 import aockt.util.asIntListSeperatedBySpaces
 import aockt.util.pow
+import aockt.util.splitBySpaces
 import io.github.jadarma.aockt.core.Solution
+
+typealias CardDek = MutableList<Int>
 
 object Y2023D04 : Solution {
     override fun partOne(input: String): Any {
-        val scratchcards = input.lines().map { it.toScratchcard() }
+        val scratchcards = input.asScratchCards()
         return scratchcards.sumOf(Scratchcard::points)
+    }
+
+    override fun partTwo(input: String): Any {
+        val scratchcards = input.asScratchCards()
+        val cardDeck = MutableList(scratchcards.size) { 1 }
+        for (scratchcard in scratchcards) {
+            updateDeckWithCard(cardDeck, scratchcard)
+        }
+        return cardDeck.totalCards()
+    }
+
+    private fun updateDeckWithCard(cardDeck: CardDek, card: Scratchcard) {
+        val winCount = card.winCount
+        val cardIndex = card.id - 1
+        val copies = cardDeck[cardIndex]
+        for (i in 1..winCount) {
+            cardDeck[cardIndex + i] += copies
+        }
+    }
+
+    private fun CardDek.totalCards(): Int = this.sum()
+
+    private fun String.asScratchCards(): List<Scratchcard> {
+        return this.lines().map(String::toScratchcard)
     }
 }
 
@@ -23,9 +50,10 @@ data class Scratchcard(
     companion object {
         fun fromString(string: String): Scratchcard {
             val (cardString, winningNumberString) = string.split(" \\| +".toRegex())
-            val (_, numberString) = cardString.split(regex = ": +".toRegex())
+            val (cardInfoString, numberString) = cardString.split(regex = ": +".toRegex())
+            val (_, cardIdString) = cardInfoString.splitBySpaces()
             return Scratchcard(
-                id = 0, // TODO: Parse the id
+                id = cardIdString.toInt(),
                 numbers = numberString.asIntListSeperatedBySpaces(),
                 winningNumbers = winningNumberString.asIntListSeperatedBySpaces().toSet()
             )
@@ -34,10 +62,12 @@ data class Scratchcard(
 
     val points: Int
         get() {
-            val winningCount = numbers.count { it in winningNumbers }
-            return when (winningCount) {
+            return when (val winningCount = winCount) {
                 0 -> 0
                 else -> 2.pow(winningCount - 1)
             }
         }
+
+    val winCount: Int
+        get() = numbers.count { it in winningNumbers }
 }
